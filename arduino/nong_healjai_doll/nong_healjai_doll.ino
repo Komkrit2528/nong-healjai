fc59233ef09e021687e0ed5770f98998ed438801
Komkrit2528 <98868464+Komkrit2528@users.noreply.github.com>
2026-06-08 15:37:45 +0700

    Remove Stepper library dependency

--- arduino/nong_healjai_doll/nong_healjai_doll.ino
+++ arduino/nong_healjai_doll/nong_healjai_doll.ino
@@ -1,22 +1,3 @@
-/*
-  nong healjai doll
-
-  Board: Arduino Nano
-  Audio: HW-188 style MP3 player board with Play/Pause button
-  Motor: 28BYJ-48 5V stepper motor with ULN2003 driver board
-
-  Behavior:
-  - Press the pull-up button once.
-  - Arduino simulates one Play/Pause button press on the MP3 board.
-  - The 28BYJ-48 turns forward 180 degrees, then returns 180 degrees.
-
-  MP3 trigger wiring:
-  Use a small NPN transistor or optocoupler across the MP3 board Play/Pause
-  button contacts. Arduino D7 drives the transistor/opto input.
-*/
-
-#include <Stepper.h>
-
 const byte BUTTON_PIN = 2;
 const byte MP3_PLAY_TRIGGER_PIN = 7;
 
@@ -29,35 +10,36 @@ const unsigned long DEBOUNCE_MS = 40;
 const unsigned long MP3_TRIGGER_MS = 250;
 const unsigned long START_RETURN_DELAY_MS = 300;
 
-const int STEPS_PER_REVOLUTION = 2048; // 28BYJ-48 common full output-shaft revolution.
-const int HALF_TURN_STEPS = STEPS_PER_REVOLUTION / 2;
-const int MOTOR_RPM = 10;
-
-Stepper dollMotor(
-  STEPS_PER_REVOLUTION,
-  MOTOR_IN1_PIN,
-  MOTOR_IN3_PIN,
-  MOTOR_IN2_PIN,
-  MOTOR_IN4_PIN
-);
+const int HALF_TURN_STEPS = 1024; // 28BYJ-48: about 180 degrees.
+const unsigned int STEP_DELAY_MS = 3;
 
 bool lastButtonReading = HIGH;
 bool stableButtonState = HIGH;
 unsigned long lastDebounceAt = 0;
 bool actionRunning = false;
 
+const byte STEP_SEQUENCE[8][4] = {
+  {1, 0, 0, 0},
+  {1, 1, 0, 0},
+  {0, 1, 0, 0},
+  {0, 1, 1, 0},
+  {0, 0, 1, 0},
+  {0, 0, 1, 1},
+  {0, 0, 0, 1},
+  {1, 0, 0, 1}
+};
+
 void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(MP3_PLAY_TRIGGER_PIN, OUTPUT);
-  digitalWrite(MP3_PLAY_TRIGGER_PIN, LOW);
 
   pinMode(MOTOR_IN1_PIN, OUTPUT);
   pinMode(MOTOR_IN2_PIN, OUTPUT);
   pinMode(MOTOR_IN3_PIN, OUTPUT);
   pinMode(MOTOR_IN4_PIN, OUTPUT);
-  releaseMotor();
 
-  dollMotor.setSpeed(MOTOR_RPM);
+  digitalWrite(MP3_PLAY_TRIGGER_PIN, LOW);
+  releaseMotor();
 
   Serial.begin(9600);
   Serial.println(F("nong healjai doll ready."));
@@ -94,9 +76,9 @@ void playVoiceAndMoveMotor() {
 
   triggerMp3PlayButton();
 
-  dollMotor.step(HALF_TURN_STEPS);
+  moveMotor(HALF_TURN_STEPS);
   delay(START_RETURN_DELAY_MS);
-  dollMotor.step(-HALF_TURN_STEPS);
+  moveMotor(-HALF_TURN_STEPS);
 
   releaseMotor();
   actionRunning = false;
@@ -106,6 +88,33 @@ void triggerMp3PlayButton() {
   digitalWrite(MP3_PLAY_TRIGGER_PIN, HIGH);
   delay(MP3_TRIGGER_MS);
   digitalWrite(MP3_PLAY_TRIGGER_PIN, LOW);
+}
+
+void moveMotor(int steps) {
+  int direction = 1;
+
+  if (steps < 0) {
+    direction = -1;
+    steps = -steps;
+  }
+
+  for (int i = 0; i < steps; i++) {
+    int sequenceIndex = i % 8;
+
+    if (direction < 0) {
+      sequenceIndex = 7 - sequenceIndex;
+    }
+
+    setMotorStep(sequenceIndex);
+    delay(STEP_DELAY_MS);
+  }
+}
+
+void setMotorStep(int sequenceIndex) {
+  digitalWrite(MOTOR_IN1_PIN, STEP_SEQUENCE[sequenceIndex][0]);
+  digitalWrite(MOTOR_IN2_PIN, STEP_SEQUENCE[sequenceIndex][1]);
+  digitalWrite(MOTOR_IN3_PIN, STEP_SEQUENCE[sequenceIndex][2]);
+  digitalWrite(MOTOR_IN4_PIN, STEP_SEQUENCE[sequenceIndex][3]);
 }
 
 void releaseMotor() {
